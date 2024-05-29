a5ef8ff7a10dd5bfec959286dbd76cf2349feba3
S3RA0S <artursoaressp@gmail.com>
2024-05-29 19:51:19 -0300

    Calculos das Funcoes

--- PBL_1_Semestre_Integral/Services/Calculo.cs
+++ PBL_1_Semestre_Integral/Services/Calculo.cs
@@ -8,5 +8,25 @@ namespace PBL_1_Semestre_Integral.Services
 {
     internal class Calculo
     {
+        static double FuncaoSegundoGrau(double x, double a, double b, double c)
+        {
+            return a * x * x + b * x + c;
+        }
+        static double FuncaoSeno(double x, double a, double b, double c, double d)
+        {
+            return a * Math.Pow(Math.Sin(b * x), d) + c;
+        }
+        static double FuncaoCosseno(double x, double a, double b, double c, double d)
+        {
+            return a * Math.Pow(Math.Cos(b * x), d) + c;
+        }
+        static double FuncaoLog(double x, double a, double b, double c, double d)
+        {
+            return a * Math.Pow(Math.Log10(b * x), d) + c;
+        }
+        static double FuncaoLN(double x, double a, double b, double c, double d)
+        {
+            return a * Math.Pow(Math.Log(b * x), d) + c;
+        }
     }
 }
